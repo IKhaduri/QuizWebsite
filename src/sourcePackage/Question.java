5ba664ef0b5c2f1b7aa2734a8a44531c623f1bd5
BekaMaisuradze <fcb10lal24@gmail.com>
2016-06-03 17:57:21 +0400

    Question interface has become class with some fields, created question type enumeration

--- src/sourcePackage/Question.java
+++ src/sourcePackage/Question.java
@@ -3,18 +3,35 @@ package sourcePackage;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface Question {
+public class Question {
+	
+	private QuestionType type;
+	private String question_text;
+	private String picture_link;	// for picture-response type questions
+	private ArrayList<String> correct_answers;	
+	
 	
 	/**
 	 * @return ArrayList of answers, since it might have multiple answers
 	 */
-	public List<String> getAnswers();
+	public List<String> getAnswers() {
+		
+		return null;
+	}
+	
 	/**
 	 * @return Question text to be displayed
 	 */
-	public String getQuestion();
+	public String getQuestion() {
+		
+		return "";
+	}
+	
 	/**
 	 * @return Image source or null if not exists 
 	 */
-	public String getImage();
+	public String getImage() {
+		
+		return "";
+	}
 }
---
+++ src/sourcePackage/QuestionType.java
@@ -0,0 +1,13 @@
+package sourcePackage;
+
+public enum QuestionType {
+
+	TEXT_RESPONSE,
+	
+	FILL_BLANKS,
+	
+	MULTIPLE_CHOICE,
+	
+	PICTURE_RESPONSE	
+	
+}
