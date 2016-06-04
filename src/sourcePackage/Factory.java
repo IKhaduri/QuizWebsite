aea8816a14532bddbf82eab047b4014ca428d093
IKhaduri <iraklixaduri@gmail.com>
2016-06-04 14:57:01 +0400

    Factory added connection and added MyDBInfo

--- src/sourcePackage/Factory.java
+++ src/sourcePackage/Factory.java
@@ -1,5 +1,10 @@
 package sourcePackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Factory {
 	
 	public static Database getDBObject() {
@@ -10,4 +15,21 @@ public class Factory {
 		return new User(username, username);
 	}
 	
+	public static Connection getConnection(){
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(MyDBInfo.MYSQL_DATABASE_SERVER, MyDBInfo.MYSQL_USERNAME,
+					MyDBInfo.MYSQL_PASSWORD);
+			connection.setCatalog(MyDBInfo.MYSQL_DATABASE_NAME);
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC driver not found");
+			e.printStackTrace();
+
+		}
+		return connection;
+	}
 }
