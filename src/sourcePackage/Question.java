1fb3b9f694ad4a90ebab331c3fc21f58f8c7e482
BekaMaisuradze <fcb10lal24@gmail.com>
2016-06-06 17:31:32 +0400

    Question class implemented

--- src/sourcePackage/Question.java
+++ src/sourcePackage/Question.java
@@ -3,7 +3,7 @@ package sourcePackage;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-@SuppressWarnings("unused")
+
 public class Question implements Serializable {
 	
 	/**
@@ -17,27 +17,43 @@ public class Question implements Serializable {
 	private ArrayList<String> correct_answers;	
 	
 	
+	public Question(QuestionType type, String text, String picture_link, ArrayList<String> answers) {
+		
+		this.type = type;
+		question_text = text;
+		this.picture_link = picture_link;
+		correct_answers = answers;
+	}
+	
+	/**
+	 * optional constructor where type isn't specific, and is taken 
+	 * 'EXT_RESPONSE' type as default.
+	 * @param text - Question text.
+	 * @param answers - correct answer(s).
+	 */
+	public Question(String text, ArrayList<String> answers) {
+		this(QuestionType.TEXT_RESPONSE, text, null, answers);
+	}
+	
+	
 	/**
 	 * @return ArrayList of answers, since it might have multiple answers
 	 */
-	public List<String> getAnswers() {
-		
-		return null;
-	}
+	public List<String> getAnswers() { return correct_answers; }
 	
 	/**
 	 * @return Question text to be displayed
 	 */
-	public String getQuestion() {
-		
-		return "";
-	}
+	public String getQuestion() { return question_text; }
 	
 	/**
 	 * @return Image source or null if not exists 
 	 */
 	public String getImage() {
 		
-		return "";
+		if (type == QuestionType.PICTURE_RESPONSE)
+			return picture_link;
+		
+		return null;
 	}
 }
