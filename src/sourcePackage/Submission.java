f9596e4443f79862f2325b1e1ea8a059b4dc3b71
TheDonsky <nikadon.net@gmail.com>
2016-06-07 15:56:50 +0400

    Added Submission class

---
+++ src/sourcePackage/Submission.java
@@ -0,0 +1,52 @@
+package sourcePackage;
+
+import java.sql.*;
+
+public class Submission {
+	private QuizBase quiz;
+	private Timestamp startTime;
+	private Timestamp endTime;
+	private int score;
+	
+	/**
+	 * Constructor
+	 * @param quiz - quiz that was taken
+	 * @param timeStart - date, when the user started the quiz
+	 * @param timeEnd - final submission date
+	 * @param score - score, the user got
+	 */
+	public Submission(QuizBase quiz, Timestamp timeStart, Timestamp timeEnd, int score){
+		this.quiz = quiz;
+		this.startTime = timeStart;
+		this.endTime = timeEnd;
+		this.score = score;
+	}
+	
+	/**
+	 * @return the quiz taken
+	 */
+	public QuizBase getQuiz(){
+		return(quiz);
+	}
+	
+	/**
+	 * @return submission date
+	 */
+	public Timestamp getSubmissionTime(){
+		return(endTime);
+	}
+	
+	/**
+	 * @return time it took the user to complete the quiz (in milliseconds)
+	 */
+	public long getSessionDuration(){
+		return(endTime.getTime() - startTime.getTime());
+	}
+	
+	/**
+	 * @return submission score
+	 */
+	public int getScore(){
+		return(score);
+	}
+}
