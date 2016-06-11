1d9a2dc7ec8593a87af41f218ce3095a18ec4771
TheDonsky <nikadon.net@gmail.com>
2016-06-11 14:42:51 +0400

    added Message class

---
+++ src/sourcePackage/Message.java
@@ -0,0 +1,60 @@
+package sourcePackage;
+
+import java.sql.Timestamp;
+
+public class Message {
+	private String sender;
+	private String receiver;
+	private String message;
+	private Timestamp date;
+	private boolean seen;
+	
+	/**
+	 * Constructor
+	 * @param sender - sender user name
+	 * @param receiver - receiver user name
+	 * @param message - message text
+	 * @param date - delivery date
+	 */
+	public Message(String sender, String receiver, String message, Timestamp date){
+		this.sender = sender;
+		this.receiver = receiver;
+		this.message = message;
+		this.date = date;
+	}
+	
+	/**
+	 * @return sender
+	 */
+	public String getSender(){
+		return sender;
+	}
+	
+	/**
+	 * @return receiver
+	 */
+	public String getReceiver(){
+		return receiver;
+	}
+	
+	/**
+	 * @return message text
+	 */
+	public String getMessage(){
+		return message;
+	}
+	
+	/**
+	 * @return delivery date
+	 */
+	public Timestamp getDate(){
+		return date;
+	}
+	
+	/**
+	 * @return true, if the message is seen
+	 */
+	public boolean isSeen(){
+		return seen;
+	}
+}
