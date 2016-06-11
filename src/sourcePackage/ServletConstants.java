f6e564e9c76593f367aa41714b7f484557ab6a21
IKhaduri <iraklixaduri@gmail.com>
2016-06-11 14:15:42 +0400

    new constant

--- src/sourcePackage/ServletConstants.java
+++ src/sourcePackage/ServletConstants.java
@@ -4,5 +4,6 @@ public class ServletConstants {
 	public static final String QUIZ_PARAMETER_NAME = "quizName";
 	public static final String USER_PARAMETER_NAME = "userName";
 	public static final long  BEGINNING_DATE = 1465636711761L;
+	public static final long TWENTY_FOUR_HOURS_IN_MILLIS = 86400000L;
 	
 }
