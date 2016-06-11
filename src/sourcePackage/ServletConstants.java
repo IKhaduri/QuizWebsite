cb02537375a59eac7c18f6180bca69c2c7694100
IKhaduri <iraklixaduri@gmail.com>
2016-06-11 13:26:18 +0400

    constants

---
+++ src/sourcePackage/ServletConstants.java
@@ -0,0 +1,8 @@
+package sourcePackage;
+
+public class ServletConstants {
+	public static final String QUIZ_PARAMETER_NAME = "quizName";
+	public static final String USER_PARAMETER_NAME = "userName";
+	public static final long  BEGINNING_DATE = 1465636711761L;
+	
+}
