dc7adb86035f7790327fbef88557e31cf0433af6
BekaMaisuradze <fcb10lal24@gmail.com>
2016-06-11 13:20:17 +0400

    fixed common bugs and added getNumOfSubmissions() in User class

--- src/sourcePackage/User.java
+++ src/sourcePackage/User.java
@@ -21,18 +21,32 @@ public class User {
 	}
 	
 	public double getMaxScorePercentage(Database base, Connection connection) {
+		if (base == null) return 0.0;
+		
 		return base.getUserMaxScore(this.userName, connection);
 	}
 	
 	public int getTotalScore(Database base, Connection connection) {
+		if (base == null) return 0;
+		
 		return base.getUserTotalScore(this.userName, connection);
 	}
 	
+	public int getNumOfSubmissions(Database base, Connection connection) {
+		if (base == null) return 0;
+		
+		return base.getNumOfSubmissions(this.userName, connection);
+	}
+	
 	public List<Submission> getSubmissions(Database base, Connection connection, int limit) {
+		if (base == null || limit <= 0) return null;
+		
 		return base.getUserSubmissions(this.userName, limit, connection);
 	}
 	
 	public List<QuizBase> getCreatedQuizzes(Database base, Connection connection, int limit) {
+		if (base == null || limit <= 0) return null;
+		
 		return base.getUserCreatedQuizzes(this.userName, limit, connection);
 	}
 	
