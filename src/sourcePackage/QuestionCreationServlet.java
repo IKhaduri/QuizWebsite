813098873afda65ce0eca950e989d6c592900d2b
BekaMaisuradze <fcb10lal24@gmail.com>
2016-06-18 15:25:49 +0400

    question created according to creator input from QuestionCreation page after submits the question

--- src/sourcePackage/QuestionCreationServlet.java
+++ src/sourcePackage/QuestionCreationServlet.java
@@ -2,6 +2,7 @@ package sourcePackage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,24 +35,29 @@ public class QuestionCreationServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		User user = (User) session.getAttribute(SessionListener.USER_IN_SESSION);
 		
 		@SuppressWarnings("unchecked")
 		ArrayList<QuestionAbstract> questions = (ArrayList<QuestionAbstract>) session.getAttribute(ServletConstants.CREATED_QUESTIONS);
-		if (questions == null)
+		if (questions == null) {
 			questions = new ArrayList<QuestionAbstract>();
+			session.setAttribute(ServletConstants.CREATED_QUESTIONS, questions);
+		}
 		
 		String question_text = request.getParameter("question_area");
-		String[] question_answers = request.getParameterValues("answer(s)");
+		ArrayList<String> question_answers = new ArrayList<>(Arrays.asList(request.getParameterValues("answer(s)")));
 		String question_type = request.getParameter("radio");
+		QuestionAbstract result;
 		
 		if (question_type.equals("Multiple-choice")) {
-			
+			result = new MultipleChoiceQuestion(question_text, question_answers);
 		} else if (question_type.equals("Picture-response")) {
-			
+			String picture_link = request.getParameter("picture_link");
+			result = new PictureResponseQuestion(question_text, picture_link, question_answers);
 		} else {
-			
+			result = new TextResponseQuestion(question_text, question_answers);
 		}
+		
+		questions.add(result);
 	}
 
 }
