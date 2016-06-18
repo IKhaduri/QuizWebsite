b7078465870fb573cda91ca1ad5d60268507b155
TheDonsky <nikadon.net@gmail.com>
2016-06-18 18:36:41 +0400

    fixed a bug

--- src/sourcePackage/ContextInitializer.java
+++ src/sourcePackage/ContextInitializer.java
@@ -2,7 +2,9 @@ package sourcePackage;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class ContextInitializer implements ServletContextListener {
 	
 	public static final String DATABASE_ATTRIBUTE_NAME = "db_attribute_for_context";
